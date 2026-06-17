ebe040dedf55e502444414670a5c19f66886edde
kuromamekuromame88 <tei8sia@outlook.jp>
2026-06-17 16:41:19 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-function clear_title(){
-  const title = document.getElementsByClassName("title_start")[0];
-  // 初期状態で要素を透明にする
-  title.style.opacity = "1";
-  // フェードインのアニメーションを設定
-  title.style.transition = "opacity 1s ease-in-out";
-  // アニメーションを開始
-  title.style.opacity = "0";
+
+//フェードアウト
+function feed_out(e){
+  e.style.opacity = "1";
+  e.style.transition = "opacity 1s ease-in-out";
+  e.style.opacity = "0";
 }
+
+function title_patch(){
+  const title = document.getElementsByClassName("title_start")[0];
+  title.addEventListener("click", ()=>{
+    feed_out(title);
+  });
+}
+title_patch();
